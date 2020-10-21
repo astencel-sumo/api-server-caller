e0fcb336e6b7a414cccd90523c81e4a26815068a
Andrzej Stencel <astencel@sumologic.com>
2020-10-21 07:14:37 +0000

    Query Kubernetes API server

--- script.rb
+++ script.rb
@@ -1,9 +1,26 @@
+require 'pp'
 require 'rest-client'
 
+token = File.read('/var/run/secrets/kubernetes.io/serviceaccount/token')
+puts 'API Token: '
+p token
+
 loop do
-  response = RestClient.get 'https://time.gov/'
-  puts Time.now
-  puts response.code
-  puts response.body
-  sleep(5)
+  response = RestClient::Request.execute(
+    method: :get,
+    url: 'https://kubernetes.default:443/api',
+    ssl_ca_file: '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt',
+    headers: {
+      :Authorization => "Bearer #{token}",
+      :Connection => "keep-alive"
+    }
+  ) 
+  puts 'Time:'; p Time.now
+  puts 'Request headers:'; p response.request.headers
+  puts 'Response code:'; p response.code
+  puts 'Response headers:'; pp response.headers
+  puts 'Response length:'; p response.body.length
+  puts 'Response:'; p response.body
+  puts
+  sleep(1)
 end
